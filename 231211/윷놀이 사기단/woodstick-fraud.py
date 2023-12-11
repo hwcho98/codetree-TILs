caebe3acbfab6bf3edd52d7baff3b2ab9cb70017
hwcho98 <98579168+hwcho98@users.noreply.github.com>
2023-12-11 22:31:34 +0900

    Update woodstick-fraud.py

--- 231211/윷놀이 사기단/woodstick-fraud.py
+++ 231211/윷놀이 사기단/woodstick-fraud.py
@@ -1,7 +1,3 @@
-import sys
-# sys.stdin = open("input.txt", "r")
-input = sys.stdin.readline
-
 # board idx, pedding -1 behind for goal
 bd = (
     (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,31,-1,-1,-1,-1,-1),
